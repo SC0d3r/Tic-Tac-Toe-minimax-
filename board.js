e3971551ee4e69884bd0402ce17bd368e5079de8
SC0d3r <sasan.kelishani@gmail.com>
2018-05-02 02:29:06 +0430

    Update board : clean up

--- board.js
+++ board.js
@@ -1,11 +1,6 @@
 class Board{
   constructor(){
     this._cells = Array(9).fill(null);
-    // this._cells[0] = aiPlayer;
-    // this._cells[6] = aiPlayer;
-    // this._cells[4] = humanPlayer;
-    // this._cells[2] = humanPlayer;
-
   }
   place(index , player){
     if(this._cells[index] !== null) return -1;
